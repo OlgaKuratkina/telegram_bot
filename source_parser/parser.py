cf5f856fd8fc7daf8029ec8acbdb5551a2132f9e
Olga Kuratkina <olga.kuratkina@gmail.com>
2020-10-10 17:17:16 +0300

    parse quote body

--- source_parser/parser.py
+++ source_parser/parser.py
@@ -1,6 +1,8 @@
 import aiohttp
 import asyncio
 
+from bs4 import BeautifulSoup, NavigableString
+
 BASH_URL = 'http://bash.im/random'
 
 
@@ -9,14 +11,24 @@ async def fetch(session, url):
         return await response.text()
 
 
-def parse(html_body):
-    pass
+def parse(html_body: str) -> str:
+    output = []
+    soup = BeautifulSoup(html_body, 'html.parser')
+    for element in soup.find("div", class_="quote__body").children:
+        if type(element) == NavigableString:
+            output.append(element.string.strip())
+        elif element.name == "br":
+            output.append("\n")
+
+    return "".join(output)
 
 
 async def main():
     async with aiohttp.ClientSession() as session:
         html = await fetch(session, BASH_URL)
-        print(html)
+        quote = parse(html)
+        print(quote)
+
 
 if __name__ == '__main__':
     loop = asyncio.get_event_loop()
