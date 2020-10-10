b1c1a64b96ea12707b9dc598b73235266213ec97
Olga Kuratkina <olga.kuratkina@gmail.com>
2020-10-10 17:40:22 +0300

    parse quote number, date and votes

--- source_parser/parser.py
+++ source_parser/parser.py
@@ -2,6 +2,7 @@ import aiohttp
 import asyncio
 
 from bs4 import BeautifulSoup, NavigableString
+from bs4.element import Tag
 
 BASH_URL = 'http://bash.im/random'
 
@@ -11,16 +12,42 @@ async def fetch(session, url):
         return await response.text()
 
 
-def parse(html_body: str) -> str:
+def parse_body(soup: Tag) -> str:
     output = []
-    soup = BeautifulSoup(html_body, 'html.parser')
-    for element in soup.find("div", class_="quote__body").children:
+    # soup = BeautifulSoup(html_body, 'html.parser')
+    for element in soup.children:
         if type(element) == NavigableString:
             output.append(element.string.strip())
         elif element.name == "br":
             output.append("\n")
 
     return "".join(output)
+
+
+def parse_quote_number(soup: Tag) -> str:
+    number = soup.find("a", class_="quote__header_permalink")
+    return number.string.strip()
+
+
+def parse_quote_votes(soup: Tag) -> str:
+    number = soup.find("div", class_="quote__total")
+    return number.string.strip()
+
+
+def parse_quote_date(soup: Tag) -> str:
+    number = soup.find("div", class_="quote__header_date")
+    return number.string.strip()
+
+
+def parse(html_body: str) -> str:
+    # TODO: catch exception for unexpected structure of quotes
+    soup = BeautifulSoup(html_body, 'html.parser')
+    quote = soup.find("div", class_="quote__frame")
+    quote_number = parse_quote_number(quote.find("header"))
+    quote_date = parse_quote_date(quote.find("header"))
+    quote_votes = parse_quote_votes(quote.find("footer"))
+    quote_body = parse_body(quote.find("div", class_="quote__body"))
+    return f"{quote_number} [{quote_votes} votes]\n{quote_date}\n{quote_body}"
 
 
 async def main():
--- tests/test_parser.py
+++ tests/test_parser.py
@@ -1,14 +1,10 @@
 from source_parser.parser import parse
 
 
-def test_parse():
-    with open('data/random_page.html') as f:
-        assert parse(f) == 'Kotyara: Российские смесители - от обморожения до ожога один шаг!'
-
-
-def test_parse_multiple_lines():
-    with open('data/random_page2.html') as f:
-        assert parse(f) == """Как-то давно знакомый позвонил и говорит - год назад ты собирал мне комп, не мог бы заменить сдром на более удобный?
+quote1 = "#414563 [15823 votes]\n05.12.2011 в  9:12\nKotyara: Российские смесители - от обморожения до ожога один шаг!"
+quote2 = """#438212 [8275 votes]
+02.03.2016 в  9:44
+Как-то давно знакомый позвонил и говорит - год назад ты собирал мне комп, не мог бы заменить сдром на более удобный?
 - не читает сд?
 - читает.
 - не читает двд?
@@ -20,4 +16,14 @@ def test_parse_multiple_lines():
 
 Привёз он комп, оказалось - сдром кверху ногами установлен. (собирал-то боком системный блок, на столе)
 Перевернул - и человек снова доволен - теперь диск не надо придерживать когда вставляешь. Да и не падает, когда выезжает."""
+
+
+def test_parse():
+    with open('data/random_page.html') as f:
+        assert parse(f) == quote1
+
+
+def test_parse_multiple_lines():
+    with open('data/random_page2.html') as f:
+        assert parse(f) == quote2
 
